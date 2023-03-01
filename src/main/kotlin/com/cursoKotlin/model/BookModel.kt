7194ca115fd6f2d8d90068a5bdb4143570e8976b
emicarvalho <emily.carvalho@mercadolivre.com>
2023-03-01 11:40:28 -0300

    setting change permissions in BookStatus

--- src/main/kotlin/com/cursoKotlin/model/BookModel.kt
+++ src/main/kotlin/com/cursoKotlin/model/BookModel.kt
@@ -13,7 +13,7 @@ import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
 @Entity(name = "book")
-data class BookModel (
+data class BookModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int? = null,
@@ -24,11 +24,24 @@ data class BookModel (
     @Column
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? =null,
-
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null
-)
+) {
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null
+        set(value) {
+            if (field == BookStatus.CANCELADO || field == BookStatus.DELETADO)
+                throw Exception("Não é possível alterar um livro com status ${field}")
+            field = value
+        }
+
+    constructor(id: Int? = null,
+                name: String,
+                price: BigDecimal,
+                status: BookStatus?,
+                customer: CustomerModel? = null): this(id, name, price, customer) {
+                    this.status = status
+                }
+}
